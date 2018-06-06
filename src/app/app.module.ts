4c4b737b4f99183a365131ea84b48754cc8568cd
Kieran Chadwick <kieran.chadwick@outlook.com>
2018-06-06 16:53:05 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AmplifyAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
